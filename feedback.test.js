7eef2a1732a66f98965da4be44f98307b28e1e8c
Patrik <pat_p3@hotmail.com>
2025-03-20 12:57:29 +0100

    Added tests for input validation, conversion and returns an array of objects

--- feedback.test.js
+++ feedback.test.js
@@ -7,16 +7,17 @@ Test #Number(Letter)
 [x] #1 Check that both string inputs (guessed word and answer) have the same length.
     -if not, return error.
 [x] #2 Convert guessed word and answer to uppercase.
-[] #3 Only use 'A-Ö' characters, no symbols or numbers.
-[] #4 Convert every letter to an array.
-[] #5 Compare each letter in guessed word to each letter in the answer.
-    [] #5a - if the letter is correct and on the correct slot, mark as 'correct'.
-    [] #5b - if the letter is incorrect, mark as 'incorrect'.
-    [] #5c - if the letter is correct but on the wrong slot, mark as 'misplaced'.
-[] #6 if guessed letter occur more times than in the answer.
+[x] #3 Only use alphabetical characters in guessWord, no special characters.
+[x] #4 Only use alphabetical characters in answer, no special characters.
+[x] #5 Convert every letter to an array.
+[] #6 Compare each letter in guessed word to each letter in the answer.
+    [] #6a - if the letter is correct and on the correct slot, mark as 'correct'.
+    [] #6b - if the letter is incorrect, mark as 'incorrect'.
+    [] #6c - if the letter is correct but on the wrong slot, mark as 'misplaced'.
+[] #7 if guessed letter occur more times than in the answer.
     [] #6a - mark correct amount of letter as 'correct' or 'misplaced'.
     [] #6b - mark excess letters as 'incorrect'.
-[] #7 Return an array of objects for every letter.
+[] #8 Return an array of objects for every letter.
     [] #7a - every object should have a result attribute.
     
     Example:    [
@@ -28,12 +29,36 @@ Test #Number(Letter)
 
 describe('feedback()', () => {
     test('#1 Throw an error if guessed word and answer have different lengths', () => {
+        
         expect(() => feedback('CYKLA', 'CYKLADE')).toThrow('Words must be the same length');
     });
 
-    test('#2 Converts guessed word and answer to uppercase', () => {
-        const result = feedback('cykla', 'hallå');
+    test('#2 Convert guessed word and answer to uppercase', () => {
+        const result = feedback('cykla', 'maska');
+        const letters = result.map(item => item.letter).join('');
 
-        expect(result).toEqual({ guessWord: 'CYKLA', answer: 'HALLÅ' });
+        expect(letters).toBe('CYKLA');
+    });
+
+    test('#3 Remove invalid characters from guessWord', () => {
+        const guessWordresult = feedback('cykl#', 'cykl');
+        const letters = guessWordresult.map(item => item.letter).join('');
+       
+        expect(letters).toEqual('CYKL');
+    });
+
+    test('#4 Remove invalid characters from answer', () => {
+        const result = feedback('cykla', 'm@as!ka');
+        const cleanedAnswer = 'm@as!ka'.replace(/\W/g, '').toUpperCase();
+        
+        expect(result.length).toBe(cleanedAnswer.length);
+    });
+
+    test('#5 Convert guessed word and answer to arrays', () => {
+        const result = feedback('cykla', 'maska');
+    
+        expect(Array.isArray(result)).toBe(true);
+        expect(result.length).toBe(5);
+        expect(result[0]).toHaveProperty('letter');
     });
 });
