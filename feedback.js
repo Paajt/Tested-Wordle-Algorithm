3150b549cfd1eb109ed40adfd6a7ba88e240c823
Patrik <pat_p3@hotmail.com>
2025-03-20 10:10:44 +0100

    Checklist structure, words length and uppercase tests

--- feedback.js
+++ feedback.js
@@ -1,2 +1,11 @@
-export default function feedback() {
+export default function feedback(guessWord, answer) {
+
+    if (guessWord.length !== answer.length) {
+        throw new Error('Words must be the same length');
+    }
+
+    guessWord = guessWord.toUpperCase();
+    answer = answer.toUpperCase();
+
+    return { guessWord, answer};
 }
--- feedback.test.js
+++ feedback.test.js
@@ -1,22 +1,23 @@
 import feedback from "./feedback";
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
 
 /* 
+Test #Number(Letter)
 
-[] Check that both string inputs (guessed word and answer) have the same length.
+[x] #1 Check that both string inputs (guessed word and answer) have the same length.
     -if not, return error.
-[] Convert every word to uppercase.
-[] Only use 'A-Ö' characters, no symbols or numbers.
-[] Convert every letter to an array.
-[] Compare each letter in guessed word to each letter in the answer.
-    []- if the letter is correct and on the correct slot, mark as 'correct'.
-    []- if the letter is incorrect, mark as 'incorrect'.
-    []- if the letter is correct but on the wrong slot, mark as 'misplaced'.
-    []- if guessed letter occur more times than in the answer.
-        []- mark correct amount of letter as 'correct' or 'misplaced'.
-        []- mark excess letters as 'incorrect'.
-[] Return an array of objects for every letter.
-    []- every object should have a result attribute.
+[x] #2 Convert guessed word and answer to uppercase.
+[] #3 Only use 'A-Ö' characters, no symbols or numbers.
+[] #4 Convert every letter to an array.
+[] #5 Compare each letter in guessed word to each letter in the answer.
+    [] #5a - if the letter is correct and on the correct slot, mark as 'correct'.
+    [] #5b - if the letter is incorrect, mark as 'incorrect'.
+    [] #5c - if the letter is correct but on the wrong slot, mark as 'misplaced'.
+[] #6 if guessed letter occur more times than in the answer.
+    [] #6a - mark correct amount of letter as 'correct' or 'misplaced'.
+    [] #6b - mark excess letters as 'incorrect'.
+[] #7 Return an array of objects for every letter.
+    [] #7a - every object should have a result attribute.
     
     Example:    [
                     { letter: 'X', result: 'correct' },
@@ -24,3 +25,15 @@ import { describe, expect, it } from "@jest/globals";
                     { letter: 'Z', result: 'misplaced' } 
                 ]
 */
+
+describe('feedback()', () => {
+    test('#1 Throw an error if guessed word and answer have different lengths', () => {
+        expect(() => feedback('CYKLA', 'CYKLADE')).toThrow('Words must be the same length');
+    });
+
+    test('#2 Converts guessed word and answer to uppercase', () => {
+        const result = feedback('cykla', 'hallå');
+
+        expect(result).toEqual({ guessWord: 'CYKLA', answer: 'HALLÅ' });
+    });
+});
